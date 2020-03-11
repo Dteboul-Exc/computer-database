0e88abbb8dbee1955173111c28b566bfb720cd12
Dteboul-Exc <dteboul@dteboul-ThinkPad-T450>
2020-03-11 10:54:15 +0100

    Restructuration of interface + BUG
    the interface has been separated in many java files.
    Error discovered E1 : When modifying a computer that had no company id,
    an Error occur; here's the SQL error for reference :
    Cannot add or update a child row: a foreign key constraint fails
    (`computer-database-db`.`computer`, CONSTRAINT `fk_computer_company_1`
    FOREIGN KEY (`company_id`) REFERENCES `company` (`id`)

---
+++ Computer-db/src/exc/ui/MainMenu.java
@@ -0,0 +1,153 @@
+package exc.ui;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import exc.mapper.state;
+import exc.model.Company;
+import exc.model.Computer;
+import exc.persistence.SQLConnect;
+import exc.ui.SecondaryMenus;
+
+public class MainMenu {
+	public static void menu() throws SQLException
+	{
+		state sta;
+		sta = state.Main;
+		Scanner reader = new Scanner(System.in);
+		Optional<Computer> compute;
+		SQLConnect a =  SQLConnect.getInstance();
+		a.connect();
+
+		while(true)
+		{
+			switch (sta) {
+			case Main:
+				System.out.println("1  List Computer");
+				System.out.println("2  List Company");
+				System.out.println("3  Show Computer detail");
+				System.out.println("4  Add Computer");
+				System.out.println("5  Modify Computer");
+				System.out.println("6  Remove Computer");
+				System.out.println("7  Exit \n");
+				BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+				
+				int s = reader.nextInt();
+				//reader.close();
+				System.out.println(s);
+				if (s == 1)
+				{
+					sta = state.LComputer; 
+				}
+				else if (s == 2)
+				{
+					sta = state.LCompany; 
+				}
+				else if (s== 3)
+				{
+					sta = state.DComputer; 					
+				}
+				else if (s==4)
+				{
+					sta =state.CreateComputer;
+				}
+				else if (s==5)
+				{
+					sta = state.UpdateComputer;
+				}
+				else if (s==6)
+				{
+					sta = state.DeleteComputer;
+				}
+				else if (s==7)
+				{
+					sta = state.Exit;
+				}
+				break;
+			case LComputer :
+				sta = state.Main;
+				List<Computer> lcomp = a.getAllComputer();
+				lcomp.forEach((i)->System.out.println("id : "+i.getId()+" name : " +i.getName() +" start :" + i.getIntroduced() + " end :" + i.getDiscontinued() + " C_id " + i.getCompany_id() ));
+				break;
+			case LCompany :
+				sta = state.Main;
+				List<Company> lcny = a.getAllCompany();
+				lcny.forEach((i)->System.out.println("id : "+i.getId()+" name : " +i.getName()));
+				break;
+			case DComputer :
+				sta = state.Main;
+				System.out.println("Enter the id of the computer you want to select");
+				s = reader.nextInt();
+				compute = a.getSpecificComputer(s);
+				if (compute.isPresent())
+				{	 
+					Computer c = compute.get();
+					System.out.println("id : "+c.getId()+" name : " +c.getName() +" start :" + c.getIntroduced() + " end :" + c.getDiscontinued() + " C_id " + c.getCompany_id());
+				}
+				else
+				{
+					System.out.println("pc not found");
+				}
+				break;
+			case CreateComputer:
+				sta = state.Main;
+				try {
+					SecondaryMenus.Computer_Add();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				break;
+			case UpdateComputer:
+				sta = state.Main;
+				System.out.println("Enter the id of the computer you want to select");
+				s = reader.nextInt();
+				try {
+					compute = a.getSpecificComputer(s);
+					if (compute.isPresent())
+					{	 
+						Computer c = compute.get();
+						try {
+							System.out.println(SecondaryMenus.Computer_Modify(c));
+						} catch (ParseException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					else
+					{
+						System.out.println("pc not found");
+					}
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				break;
+			case DeleteComputer:
+				sta = state.Main;
+				System.out.println("Enter the id of the computer you want to delete");
+				s = reader.nextInt();
+				try {
+					a.deleteSpecificComputer(s);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case Exit:
+				System.exit(0); 
+				break;
+			default:
+				break;
+			}
+		}
+	
+	}
+
+}
--- Computer-db/src/exc/ui/SecondaryMenus.java
+++ Computer-db/src/exc/ui/SecondaryMenus.java
@@ -1,137 +1,14 @@
 package exc.ui;
-import java.io.IOException;
-import java.sql.SQLException;
-import exc.ui.MainMenu;
-public class Main {
 
-	public static void main(String[] args) throws IOException, SQLException {
-		MainMenu.menu();
-	}
-	}
-		// TODO Auto-generated method stub
-/*		state sta;
-		sta = state.Main;
-		Scanner reader = new Scanner(System.in);
-		Optional<Computer> compute;
-		SQLConnect a =  SQLConnect.getInstance();
-		a.connect();
+import java.text.ParseException;
+import java.util.Scanner;
 
-		while(true)
-		{
-			switch (sta) {
-			case Main:
-				System.out.println("1  List Computer");
-				System.out.println("2  List Company");
-				System.out.println("3  Show Computer detail");
-				System.out.println("4  Add Computer");
-				System.out.println("5  Modify Computer");
-				System.out.println("6  Remove Computer");
-				System.out.println("7  Exit \n");
-				BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-				
-				int s = reader.nextInt();
-				//reader.close();
-				System.out.println(s);
-				if (s == 1)
-				{
-					sta = state.LComputer; 
-				}
-				else if (s == 2)
-				{
-					sta = state.LCompany; 
-				}
-				else if (s== 3)
-				{
-					sta = state.DComputer; 					
-				}
-				else if (s==4)
-				{
-					sta =state.CreateComputer;
-				}
-				else if (s==5)
-				{
-					sta = state.UpdateComputer;
-				}
-				else if (s==6)
-				{
-					sta = state.DeleteComputer;
-				}
-				else if (s==7)
-				{
-					sta = state.Exit;
-				}
-				break;
-			case LComputer :
-				sta = state.Main;
-				List<Computer> lcomp = a.getAllComputer();
-				lcomp.forEach((i)->System.out.println("id : "+i.getId()+" name : " +i.getName() +" start :" + i.getIntroduced() + " end :" + i.getDiscontinued() + " C_id " + i.getCompany_id() ));
-				break;
-			case LCompany :
-				sta = state.Main;
-				List<Company> lcny = a.getAllCompany();
-				lcny.forEach((i)->System.out.println("id : "+i.getId()+" name : " +i.getName()));
-				break;
-			case DComputer :
-				sta = state.Main;
-				System.out.println("Enter the id of the computer you want to select");
-				s = reader.nextInt();
-				compute = a.getSpecificComputer(s);
-				if (compute.isPresent())
-				{	 
-					Computer c = compute.get();
-					System.out.println("id : "+c.getId()+" name : " +c.getName() +" start :" + c.getIntroduced() + " end :" + c.getDiscontinued() + " C_id " + c.getCompany_id());
-				}
-				else
-				{
-					System.out.println("pc not found");
-				}
-				break;
-			case CreateComputer:
-				try {
-					Computer_Add();
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+import exc.mapper.Computermapper;
+import exc.model.Computer;
+import exc.persistence.SQLConnect;
 
-				break;
-			case UpdateComputer:
-				sta = state.Main;
-				System.out.println("Enter the id of the computer you want to select");
-				s = reader.nextInt();
-				compute = a.getSpecificComputer(s);
-				if (compute.isPresent())
-				{	 
-					Computer c = compute.get();
-					try {
-						System.out.println(Computer_Modify(c));
-					} catch (ParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				else
-				{
-					System.out.println("pc not found");
-				}
-				break;
-			case DeleteComputer:
-				sta = state.Main;
-				System.out.println("Enter the id of the computer you want to delete");
-				s = reader.nextInt();
-				a.deleteSpecificComputer(s);
-				break;
-			case Exit:
-				System.exit(0); 
-				break;
-			default:
-				break;
-			}
-		}
-	}
-
-	
-	private static int Computer_Modify(Computer computer) throws ParseException
+public class SecondaryMenus {
+	public static int Computer_Modify(Computer computer) throws ParseException
 	{
 
 		Scanner reader = new Scanner(System.in);
@@ -214,7 +91,7 @@ public class Main {
 		}
 		return 0;
 	}
-	private static int Computer_Add() throws ParseException
+	public static void Computer_Add() throws ParseException
 	{
 		Computer computer = new Computer();
 		computer.setName(null);
@@ -295,10 +172,9 @@ public class Main {
 				System.out.println(" name : " +computer.getName() +" start :" + computer.getIntroduced() + " end :" + computer.getDiscontinued() + " C_id " + computer.getCompany_id());
 				a.addComputer(computer.getName(),computer.getIntroduced(),computer.getDiscontinued(), computer.getCompany_id());
 				check= false;
-				return 0;
+				return;
 			}
 		}
-		return 0;
-	}*/
-	
+	}
 
+}
