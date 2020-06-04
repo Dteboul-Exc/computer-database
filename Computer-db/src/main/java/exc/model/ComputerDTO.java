a4eb5a3ea818daef5a88e6cc72ea926cd3c97dea
Dteboul-Exc <dteboultornezy@excilys.com>
2020-06-04 18:14:07 +0200

    Currentling creating the mapper for DTOComputer

--- Computer-db/src/main/java/exc/model/ComputerDTO.java
+++ Computer-db/src/main/java/exc/model/ComputerDTO.java
@@ -2,7 +2,17 @@ package main.java.exc.model;
 
 import java.time.LocalDate;
 
+import main.java.exc.model.Computer.Builder;
+
 public class ComputerDTO {
+	public ComputerDTO(Builder builder)
+	{
+		this.id = builder.id;
+		this.name =builder.name;
+		this.company = builder.company;
+		this.discontinued = builder.discontinued;
+		this.introduced = builder.introduced;
+	}
 	public String getId() {
 		return id;
 	}
@@ -45,4 +55,59 @@ public class ComputerDTO {
 	private String company;
 	private String introduced;
 	private String discontinued;
+	public static class Builder
+	{
+		private String id;
+		private String name;
+		private String Computer_company;
+		private String company;
+		private String introduced;
+		private String discontinued;
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getIntroduced() {
+			return introduced;
+		}
+		public void setIntroduced(String introduced) {
+			this.introduced = introduced;
+		}
+		public String getDiscontinued() {
+			return discontinued;
+		}
+		public void setDiscontinued(String discontinued) {
+			this.discontinued = discontinued;
+		}
+		public String getComputer_company() {
+			return Computer_company;
+		}
+		public void setComputer_company(String computer_company) {
+			Computer_company = computer_company;
+		}
+		public String getCompany() {
+			return company;
+		}
+		public void setCompany(String company) {
+			this.company = company;
+		}
+        public static Builder newInstance() 
+        { 
+            return new Builder(); 
+        } 
+        private Builder() {}
+
+		public ComputerDTO build()
+		{
+			return new ComputerDTO(this);
+		}
+	}
 }
