1a0984f38715247d9e67acda1d02929820876c95
Dteboul-Exc <dteboultornezy@excilys.com>
2020-06-04 14:19:56 +0200

    Continuing Testing

--- Computer-db/src/main/java/exc/mapper/DateMapper.java
+++ Computer-db/src/main/java/exc/mapper/DateMapper.java
@@ -5,10 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.List;
 import java.util.Optional;
-
-import main.java.exc.model.Computer;
 
 public class DateMapper {
 	/**
--- Computer-db/src/test/java/SQLTestingCase.java
+++ Computer-db/src/test/java/SQLTestingCase.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.dbunit.DBTestCase;
 import org.dbunit.DatabaseTestCase;
@@ -12,11 +19,21 @@ import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
+
+import main.java.exc.model.Company;
+import main.java.exc.persistence.DAOCompany;
+import main.java.exc.service.ServiceDAOCompany;
 
 public class SQLTestingCase extends DatabaseTestCase {
+	private DAOCompany DAO = new DAOCompany();
     public static final String TABLE_COMPANY = "company";
-    private static final String DBUNIT_DATA_DIR = "/test/resources/";
+
+    Optional<Company> mCompany = Optional.of( Company.Builder.newInstance().setId(13).setName("End of a century").build());
+    
+    
     private FlatXmlDataSet loadedDataSet;
 	protected IDatabaseConnection getConnection() throws Exception{
 		//Class driverClass = Class.forName("com.mysql.cj.jdbc.Driver");
@@ -30,18 +47,49 @@ public class SQLTestingCase extends DatabaseTestCase {
 	// Load the data which will be inserted for the test
 	@SuppressWarnings("deprecation")
 	protected IDataSet getDataSet() throws Exception{
-		loadedDataSet = new FlatXmlDataSet(this.getClass().getClassLoader().getResourceAsStream(DBUNIT_DATA_DIR+"testeddb.xml"));
+		
+		loadedDataSet = new FlatXmlDataSet(this.getClass().getClassLoader().getResourceAsStream("testeddb.xml"));
 		return loadedDataSet;
 	}
 	
 	
 	@Test
-	public void testCkeckDataLoaded() throws DataSetException {
+	public void testCkeckDataLoaded() throws DataSetException, SQLException, ParseException {
 	       assertNotNull(loadedDataSet);
 	       int rowCount = loadedDataSet.getTable(TABLE_COMPANY).getRowCount();
-	       assertEquals(3, rowCount);
+	       Methods m = new Methods();
+	       Optional<Company> tested = m.Company_getid(13);
+			//Optional<List<Company>> company = Optional.of(new ArrayList());
+			
+	       assertEquals(true,mCompany.get().equals(tested.get()));
+
+	      
 	}
 
+	@Test
+	public void successTestFindCompanyById() throws DataSetException, SQLException, ParseException {
+	       assertNotNull(loadedDataSet);
+	       Optional<Company> tested = DAO.getSpecificCompany(13);
+	       assertEquals(mCompany.get().getName(), tested.get().getName());
+	}
+	
+	@Test
+	public void failureTestFindCompanyById() throws DataSetException, SQLException, ParseException {
+	       assertNotNull(loadedDataSet);
+	       Optional<Company> tested = DAO.getSpecificCompany(13);
+	       assertEquals(mCompany, tested);
+	}
+	
+	@Test
+	public void successTestFindAllCompany() throws DataSetException, SQLException {
+			List<Company> list = new ArrayList<Company>();
+			list.add(Company.Builder.newInstance().setId(1).setName("Sweden").build());
+			list.add(Company.Builder.newInstance().setId(14).setName("Fin de siecle").build());
+			list.add(Company.Builder.newInstance().setId(13).setName("End of a century").build());
+	       assertNotNull(loadedDataSet);
+	       Optional<List<Company>> result = DAO.getAllCompany();
+	       assertEquals(Optional.ofNullable(list), result);
+	}
 
 
 }
