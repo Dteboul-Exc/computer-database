3f517b504b91226a784419dac1317527bd2ad5b0
Dteboul-Exc <dteboultornezy@excilys.com>
2020-06-08 14:16:17 +0200

    Added Back-end Validation

--- Computer-db/src/main/java/exc/service/ComputerValidator.java
+++ Computer-db/src/main/java/exc/service/ComputerValidator.java
@@ -1,5 +1,23 @@
 package main.java.exc.service;
 
+import main.java.exc.mapper.DateMapper;
+
 public class ComputerValidator {
 
+	public boolean isName(String name)
+	{
+		if (name.equals("") || (name.equals(null))) return false;
+		return true;
+	}
+	public boolean isDate(String introduced, String discontinued)
+	{
+		if ((DateMapper.StringConverter(introduced).get().isAfter(DateMapper.StringConverter(discontinued).get()))) {
+		return false;
+		} else if (((introduced.equals(null) || introduced.equals("") && (!discontinued.equals(null) && (!discontinued.equals(null))))))
+		{
+			return false;	
+		}
+		return true;
+		
+	}
 }
---
+++ Computer-db/src/main/java/exc/service/ValidatorException.java
@@ -0,0 +1,10 @@
+package main.java.exc.service;
+
+import java.time.LocalDate;
+
+public class ValidatorException extends Exception {
+	ValidatorException(){super();}
+	ValidatorException(String e){super(e);}
+	ValidatorException(LocalDate introduced,LocalDate dicontinued) {};
+
+}
